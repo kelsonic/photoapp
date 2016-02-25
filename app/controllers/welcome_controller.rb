acffc046a56e7c16ea8e8f2ba818e4cc7634a703
kgadams93 <kgadams93@gmail.com>
2016-02-25 20:48:27 +0000

    Add redirect to images index path when going to root path as logged-in user

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,9 @@ class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
   
   def index
+    if current_user
+      redirect_to images_path
+    end
   end
   
 end
