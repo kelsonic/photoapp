7e188aa3df1b400a35a2cfde6967ad9912e9a87f
kgadams93 <kgadams93@gmail.com>
2016-02-26 09:46:35 +0000

    Add font awesome, update nav styles, update new and show image pages styles

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,6 +3,8 @@ class Image < ActiveRecord::Base
   belongs_to :user
   mount_uploader :picture, PictureUploader
   validate :picture_size
+  validates :name, presence: true, length: { minimum: 2, maximum: 50 }
+  validates :picture, presence: true
   
   private
   
